0c768074ebea3d92bfabc87b16f134756ea69afc
wnf <william@fluxrme.com>
2023-11-30 17:29:51 -0600

    completes exercise

--- src/utilities/items-api.js
+++ src/utilities/items-api.js
@@ -1,13 +1,14 @@
+// items-api.js
+
 import sendRequest from "./send-request";
+
 const BASE_URL = '/api/items';
 
 export async function getAll() {
   return sendRequest(BASE_URL);
 }
 
-// This function is never actually used in SEI CAFE,
-// it's only provided here to remind you to follow
-// RESTful routing, etc.
+// This function is never actually used in SEI CAFE, it's only provided here to remind you to follow RESTful routing, etc.
 export async function getById(id) {
   return sendRequest(`${BASE_URL}/${id}`);
 }
